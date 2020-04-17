07d0f0c2dbc3549c3d1f80fe8748aeb21d223d4d
Diogo Tridapalli <diogot@users.noreply.github.com>
2020-04-17 15:00:53 -0300

    Initial Commit

---
+++ NimbleBug/NimbleBug.h
@@ -0,0 +1,19 @@
+//
+//  NimbleBug.h
+//  NimbleBug
+//
+//  Created by Diogo on 17/04/20.
+//  Copyright © 2020 Diogo Tridapalli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NimbleBug.
+FOUNDATION_EXPORT double NimbleBugVersionNumber;
+
+//! Project version string for NimbleBug.
+FOUNDATION_EXPORT const unsigned char NimbleBugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NimbleBug/PublicHeader.h>
+
+
